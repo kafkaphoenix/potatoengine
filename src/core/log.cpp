41d9093b64a6dea917416bfcd98effc508f06f56
kafkaphoenix <jaguilerapuerta@gmail.com>
2022-09-24 21:12:35 +0100

    upgrade-c++20-improve-cmake

--- src/core/log.cpp
+++ src/core/log.cpp
@@ -12,7 +12,7 @@ namespace potatocraft
 	{
 		std::vector<spdlog::sink_ptr> logSinks;
 		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("..\\..\\potatocraft\\logs\\potatocraft.log", true));
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("..\\..\\logs\\potatocraft.log", true));
 
 		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
 		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
